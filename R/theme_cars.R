7a675a35318fcc09c0a19b888702091b6ceccceb
mdewey-cars <mdewey@cars.com>
2022-09-20 16:03:57 -0700

    Hi world

---
+++ R/theme_cars.R
@@ -0,0 +1,14 @@
+#' A Cars Theme to ggplot2 Functions
+#'
+#' @description This function provides a wrapper around some ggplot2 theme
+#' arguments and creates a unified CARS graphing theme, along with
+#' other color functions provided in caRs
+#' @param transparent_background A boolean for whether or not to include
+#' a transparent background. Set to false if sharing a .png file
+#'
+#' @return a gglot theme
+#' @export
+#'
+theme_cars <- function(transparent_background = T) {
+
+}
