cf80a1d695f2c6903ae7d0e3fcce4b298944cd24
mdewey-cars <mdewey@cars.com>
2022-09-27 15:39:55 -0700

    Gets the skeleton of the cars palette up and running

---
+++ R/cars_palette.R
@@ -0,0 +1,12 @@
+#' A CARS themed color palette function
+#'
+#' @description This function provides a way to quickly generate a color palette
+#' which is in the cars color scheme. It draws inspiration from the `wesanderson`
+#' library and it's ability to dynamically define palettes from themes
+#' @param business_area Accepts a few different arguments based upon the business area.
+#' Defaults to cars
+#'
+#' @return a vector with hex codes of colors
+#'
+
+cars_palette <- function(business_area = c('Cars', 'DI')) {}
