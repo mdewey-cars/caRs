47e2abba637852946f1a7b33af0547999b90ea44
mdewey-cars <mdewey@cars.com>
2022-09-27 15:12:10 -0700

    Adds substance to the theme_cars function

--- R/theme_cars.R
+++ R/theme_cars.R
@@ -9,6 +9,60 @@
 #' @return a gglot theme
 #' @export
 #'
+#' @importFrom ggplot2 '%+replace%'
 theme_cars <- function(transparent_background = T) {
+  # For ease of declaration, we will establish some common fonts here, cribbed from
+  # the website's CSS
+  # cars_font <- ""
+  plot_title_text_size <- 20
+  title_text_size <- 16
+  regular_text_size <- 14
+  base_theme <- ggplot2::theme(
+      panel.grid.major = ggplot2::element_line(
+        # This is the "lighter grey" color from cars
+        color = '#e6e6e6',
+        linetype = 'solid',
+        lineend = 'square',
+        size = 0.8
+      ),
+      plot.title = ggplot2::element_text(
+        #family = cars_font,
+        size = plot_title_text_size,
+        face = 'bold',
+        hjust = 0,
+        vjust = 2
+      ),
+      plot.subtitle = ggplot2::element_text(
+        #family = cars_font,
+        size = regular_text_size,
+        face = 'italic'
+      ),
+      axis.title = ggplot2::element_text(
+        #family = cars_font,
+        size = title_text_size
+      ),
+      axis.text = ggplot2::element_text(
+        #family = cars_font,
+        size = regular_text_size
+      ),
+      legend.title = ggplot2::element_text(
+        #family = cars_font,
+        size = title_text_size
+      ),
+      legend.text = ggplot2::element_text(
+        #family = cars_font,
+        size = regular_text_size
+      ),
+      panel.border = ggplot2::element_rect(
+        fill = NA,
+        color = NA)
+    )
 
+  if (transparent_background) {
+    theme_minimal() %+replace%
+      base_theme
+  } else {
+    theme_bw() %+replace%
+      base_theme
+  }
 }
