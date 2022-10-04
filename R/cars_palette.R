63f1f1beee82344e80562011605ad0d7bc0de4bc
mdewey-cars <mdewey@cars.com>
2022-10-04 13:37:12 -0700

    Begins building out the master palette list

--- R/cars_palette.R
+++ R/cars_palette.R
@@ -1,3 +1,21 @@
+#' A Complete list of the palettes available to the cars_palette function
+#'
+#' This draws heavy inspiration from the wonderful `wesanderson` library,
+#' down to this structure to hopefully allow for easy color palette creation
+#' in some preset themes
+#'
+#'
+#' use \code{\link{\cars_palette}} to construct the palettes of desired length a given palette
+#'
+cars_palettes <- list(
+  cars_greyscale = c('white' = '#fff', 'lighter_grey' = '#e6e6e6', 'light_grey' = '#bdbdbd', 'dark_grey' = '#767676', 'black' = '#212121'),
+  cars = c('brand' = '#532380', 'accent' = '#00bfde'),
+
+)
+
+
+
+
 #' A CARS themed color palette function
 #'
 #' @description This function provides a way to quickly generate a color palette
