29e42e6e64bf3f6aca8dcfd57c2c02133ea950cd
mdewey-cars <mdewey@cars.com>
2022-11-08 15:20:11 -0800

    Adds commit skeleton

---
+++ R/setup_adobe.R
@@ -0,0 +1,19 @@
+#' setup_adobe
+#' A function to help users setup `adobeanalyticsr` to work on their machine.
+#'
+#' This function allows us to simplify away from a lot of the technical details
+#' that can sometimes confuse newcomers to adobe analytics. The `adobeanalyticsr`
+#' package provides a number of fantastic tools for analysts to pull in data
+#' from the adobe analytics workspace, thus making enabling that behavior highly
+#' desireable.
+#'
+#'
+#' @return Modifies the R session to have a working connection to `adobeanalyticsr`
+#' @export
+#'
+#' @examples
+setup_adobe <- function() {
+
+
+
+}
