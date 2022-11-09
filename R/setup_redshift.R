b868eb6710bb1183c2b560e0ac18f1305b6ae4cc
mdewey-cars <mdewey@cars.com>
2022-11-08 16:12:40 -0800

    Use of superassignment to kick out of the tryCatch with updated correct values for `credentials`

--- R/setup_redshift.R
+++ R/setup_redshift.R
@@ -1,24 +1,43 @@
 
 #' setup_redshift
 #'
-#' This function creates a connection object for working with Redshift
-#' Does a different thing depending on whether or not this is your
+#' This function creates a connection object for working with Redshift. You'll need
+#' to be on the VPN for this to work.
+#'
+#' This function does a different thing depending on whether or not this is your
 #' first time running the function. If it is, it will prompt you for login
 #' information. You can manually control whether you want to re-enter credentials
-#' with the `credentials` function argument
+#' with the `credentials` function argument, for example when you change these.
+#'
+#'
 #' @return An object named `con` to be used with calls to `DBGetQuery`
 #' @export
 #'
 #' @examples
 setup_redshift <- function(credentials = T) {
-  if !is.na(keyring::key_get('redshift_user_id')) {
-    credentials = F
-  }
-  if !credentials {
+  tryCatch(
+    keyring::key_get('redshift_user_id'),
+    error = function(e) {
+     credentials <<- F
+    }
+  )
+  if (!credentials) {
     rstudioapi::askForSecret('redshift_user_id',
-                             message = 'Enter your Redshift User ID',
+                             message = 'Enter your Redshift User ID \n
+                      All values entered will only be saved locally',
                              title = paste("Redshift Username"))
-    rstudioapi::askForSecret('redshift_password')
+    rstudioapi::askForSecret('redshift_password',
+                             message = 'Enter your Redshift Password \n
+                      All values entered will only be saved locally',
+                             title = paste("Redshift Password"))
   }
 
+  # finally, we setup a con object
+  DBI::dbConnect(odbc::odbc(),
+            Driver       = "Amazon Redshift (x64)",
+            servername   = "dw.data-prod.cars.com",
+            database     = "dw",
+            UID          = keyring::key_get('redshift_user_id'),
+            PWD          = keyring::key_get('redshift_password'),
+            Port         = 5439)
 }
